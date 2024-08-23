36c5a66034806ca72ca5b3e1f7924e35d22f8e93
LuisTerronnes01 <luis.terrones@ucsm.edu.pe>
2024-08-23 17:02:49 -0500

    cambio completo para establecer la base del proyecto

---
+++ 1_TRABAJO_COMPLETO/index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+	
+<main>
+	<h1>Bienvenido a nuestro repositorio</h1>
+	<p>Aquí colocaremos los trabajos de la asignatura <strong>DESARROLLO DE APLICACIONES 01</strong> a cargo del <strong>Dr. Mg. Ing. ANGEL FELIPE MONTESINOS MURILLO</strong>.</p>
+	<img class="imagen1" src="https://www2.ucsm.edu.pe/wp-content/uploads/2021/03/Angel-Montesinos-1.jpg" alt="Ingeniero_angel" >
+</main>
+
+<?php include 'footer.php'; ?>
---
+++ 1_TRABAJO_COMPLETO/practicas.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+	
+<main>
+	<h1>PRACTICAS</h1>
+	<section id="practica1">
+		<h2>Practica Nº1</h2>
+		<section id="experiencias1">
+			<h3>EXPERIENCIAS</h3>
+			<p>Contenido de Experiencias para Práctica 1.</p>
+			<a href="">Experiencia 1</a><br>
+			<a href="">Experiencia 2</a><br>
+			<a href="">Experiencia 3</a><br>
+
+		</section>
+
+		<section id="ejercicios1">
+			<h3>EJERCICIOS</h3>
+			<p>Contenido de EJERCICIOS para Práctica 1.</p>
+			<a href="">Ejercicio 1</a><br>
+			<a href="">Ejercicio 2</a><br>
+			<a href="">Ejercicio 3</a><br>
+		</section>
+	</section>
+
+	<section id="practica2">
+		<h2>Practica Nº2</h2>
+		<section id="experiencias2">
+			<h3>EXPERIENCIAS</h3>
+			<p>Contenido de Experiencias para Práctica 2.</p>
+			<a href="../1_TRABAJO_COMPLETO/SESION_2_CSS/ACTIVIDADES/experiencia_3/experiencia3.html">Experiencia 1</a><br>
+			<a href="">Experiencia 2</a><br>
+			<a href="">Experiencia 3</a><br>
+
+		</section>
+
+		<section id="ejercicios2">
+			<h3>EJERCICIOS</h3>
+			<p>Contenido de EJERCICIOS para Práctica 1.</p>
+			<a href="">Ejercicio 1</a><br>
+			<a href="">Ejercicio 2</a><br>
+			<a href="">Ejercicio 3</a><br>
+			
+		</section>
+	</section>
+</main>
+
+<?php include 'footer.php'; ?>
