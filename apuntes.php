0f9adeae3ae3b067d71b421eea0ab956bad638a8
LuisTerronnes01 <luis.terrones@ucsm.edu.pe>
2024-08-23 17:33:33 -0500

    actualizacion

---
+++ apuntes.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+	
+<main>
+	<h1>Apuntes</h1>
+	<p>Aquí irán los apuntes.</p>
+</main>
+
+<?php include 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+		<p>&copy; 2024 Repositorio de Prácticas. Todos los derechos reservados.</p>
+	</footer>
+</body>
+</html>
